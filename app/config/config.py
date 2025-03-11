778038980749dea17be6ed9d3dc3d70c0da0a19f
Pedro <pedro.boller2@hotmail.com>
2025-03-11 17:00:03 -0300

    First commit

--- app/config/config.py
+++ app/config/config.py
@@ -1,12 +1,21 @@
+import os
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv('app/config/.env'))
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_bucket_name = os.getenv("AWS_BUCKET_NAME")
+aws_region = os.getenv("AWS_REGION")
+
 
 class Settings(BaseSettings):
-    AWS_SECRET_ACCESS_KEY: str = "cEK7H7vKwmV9hzoGzkSvFj/Ac5tV3LO+zbHfoc/S"
-    AWS_ACCESS_KEY_ID: str = "AKIAQFLZDHWYC4LPCRGC"
-    AWS_BUCKET_NAME: str = "vivahcare-files"
-    AWS_REGION: str = "us-east-2"
+    AWS_SECRET_ACCESS_KEY: str = aws_secret_key
+    AWS_ACCESS_KEY_ID: str = aws_access_key_id
+    AWS_BUCKET_NAME: str = aws_bucket_name
+    AWS_REGION: str = aws_region
 
     class Config:
         env_file = "config/.env"
